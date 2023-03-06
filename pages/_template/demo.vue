6d0dab6bc029e3604cf27ed9384ff28ff4dd54bf
Quni <chumini0121@gmail.com>
2023-03-06 10:59:52 +0800

    commit 0306 精選finish

--- pages/_template/demo.vue
+++ pages/_template/demo.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="container">
-    <!-- 推荐 -->
     <van-sticky class="header">
       <header class="searchHeader">
         <div class="searchHeader__everyday">
@@ -23,18 +22,18 @@
       <div class="featuredTab">
         <div class="featuredTab__list">
           <ul class="list">
-            <li>
-              <a href="/_template/focus" class="btn">
+            <li :class="tab == '0' ? 'active' : ''">
+              <a href="#" class="btn" @click="tab = 0">
                 <span>关注</span>
               </a>
             </li>
-            <li class="active">
-              <a href="/_template/demo" class="btn">
+            <li :class="tab == '1' ? 'active' : ''">
+              <a href="#" class="btn" @click="tab = 1">
                 <span>推荐</span>
               </a>
             </li>
-            <li v-for="n in 10">
-              <a href="#" class="btn">
+            <li v-for="n in 10" :class="tab == '3' ? 'active' : ''">
+              <a href="#" class="btn" @click="tab = 3">
                 <span>标签</span>
               </a>
             </li>
@@ -46,55 +45,139 @@
       </div>
     </van-sticky>
     <main>
-      <section class="sectionMoney">
-        <van-swipe>
-          <van-swipe-item v-for="n in 2">
-            <img src="/_nuxt/assets/img/not-use/slider.jpg" alt="">
-          </van-swipe-item>
-        </van-swipe>
-      </section>
-      <section class="sectionButton">
-        <div class="featuredPart">
-          <a href="#" class="btn" v-for="n in 3">
-            <img :src="'/_nuxt/assets/img/btn_first_' + n + '.png'">
-          </a>
-        </div>
-      </section>
-      <section class="sectionVideo">
-        <div class="featuredVideo">
-          <div class="list">
-            <div class="item" v-for="n in 20">
-              <div class="preview">
-                <div class="title">3/1露脸女朋友 大一女生爱露脸 直播镜头 露脸女朋友</div>
-                <div class="view"><span>5</span>万次播放</div>
-                <div class="time"><span>2:12</span></div>
-                <img class="img" src="https://api.bcyapp005.com/storage/files/shares/HH/3/f533de42-4806-4108-beec-612bc6e47b5c.jpg">
-              </div>
-              <div class="info">
-                <div class="avatar">
-                  <img class="img" src="https://api.bcyapp005.com/storage/files/shares/HH/3/f533de42-4806-4108-beec-612bc6e47b5c.jpg">
-                </div>
-                <div class="title">
-                  <div class="name">
-                    <p>露脸女朋友 大一女生爱露脸 直拨镜头 露脸女朋友 露脸女朋友</p>
-                    <button class="btn">关注</button>
+      <div v-if="tab === 0">
+        <section class="sectionVideoFocus">
+          <div class="featuredVideoFocus">
+            <div class="list">
+              <div class="item" v-for="n in 20">
+                <div class="info">
+                  <div class="avatar">
+                    <img class="img" src="https://api.bcyapp005.com/storage/files/shares/HH/3/f533de42-4806-4108-beec-612bc6e47b5c.jpg">
                   </div>
-                  <span>88888888关注<i class="dot"></i>2551视频</span>
+                  <div class="title">
+                    <div class="name">
+                      <p>露脸女朋友 大一女生爱露脸 直拨镜头 露脸女朋友 露脸女朋友</p>
+                    </div>
+                    <span>2021-08-30</span>
+                  </div>
+                </div>
+                <div class="preview">
+                  <img class="img" src="https://api.bcyapp005.com/storage/files/shares/HH/3/f533de42-4806-4108-beec-612bc6e47b5c.jpg">
                 </div>
                 <div class="interaction">
                   <a href="#" class="btn">
+                    <i class="icon icon__love"></i>
+                    <span>14 132</span>
+                  </a>
+                  <a href="#" class="btn">
                     <i class="icon icon__message"></i>
-                    <span>1w</span>
+                    <span>116</span>
                   </a>
                   <a href="#" class="btn">
                     <i class="icon icon__share"></i>
+                    <span>分享</span>
                   </a>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </div>
+      <div v-else-if="tab === 1">
+        <section class="sectionMoney">
+          <van-swipe>
+            <van-swipe-item v-for="n in 2">
+              <img src="/_nuxt/assets/img/not-use/slider.jpg" alt="">
+            </van-swipe-item>
+          </van-swipe>
+        </section>
+        <section class="sectionButton">
+          <div class="featuredPart">
+            <a href="#" class="btn" v-for="n in 3">
+              <img :src="'/_nuxt/assets/img/btn_first_' + n + '.png'">
+            </a>
+          </div>
+        </section>
+        <section class="sectionVideo">
+          <div class="featuredVideo">
+            <div class="list">
+              <div class="item" v-for="n in 20">
+                <div class="preview">
+                  <div class="title">3/1露脸女朋友 大一女生爱露脸 直播镜头 露脸女朋友</div>
+                  <div class="view"><span>5</span>万次播放</div>
+                  <div class="time"><span>2:12</span></div>
+                  <img class="img" src="https://api.bcyapp005.com/storage/files/shares/HH/3/f533de42-4806-4108-beec-612bc6e47b5c.jpg">
+                </div>
+                <div class="info">
+                  <div class="avatar">
+                    <img class="img" src="https://api.bcyapp005.com/storage/files/shares/HH/3/f533de42-4806-4108-beec-612bc6e47b5c.jpg">
+                  </div>
+                  <div class="title">
+                    <div class="name">
+                      <p>露脸女朋友 大一女生爱露脸 直拨镜头 露脸女朋友 露脸女朋友</p>
+                      <button class="btn">关注</button>
+                    </div>
+                    <span>88888888关注<i class="dot"></i>2551视频</span>
+                  </div>
+                  <div class="interaction">
+                    <a href="#" class="btn">
+                      <i class="icon icon__message"></i>
+                      <span>1w</span>
+                    </a>
+                    <a href="#" class="btn">
+                      <i class="icon icon__share"></i>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+      <div v-else>
+        <section class="sectionMoney">
+          <van-swipe>
+            <van-swipe-item v-for="n in 2">
+              <img src="/_nuxt/assets/img/not-use/slider.jpg" alt="">
+            </van-swipe-item>
+          </van-swipe>
+        </section>
+        <section class="sectionVideo mt:10">
+          <div class="featuredVideo">
+            <div class="list">
+              <div class="item" v-for="n in 20">
+                <div class="preview">
+                  <div class="title">3/1露脸女朋友 大一女生爱露脸 直播镜头 露脸女朋友</div>
+                  <div class="view"><span>5</span>万次播放</div>
+                  <div class="time"><span>2:12</span></div>
+                  <img class="img" src="https://api.bcyapp005.com/storage/files/shares/HH/3/f533de42-4806-4108-beec-612bc6e47b5c.jpg">
+                </div>
+                <div class="info">
+                  <div class="avatar">
+                    <img class="img" src="https://api.bcyapp005.com/storage/files/shares/HH/3/f533de42-4806-4108-beec-612bc6e47b5c.jpg">
+                  </div>
+                  <div class="title">
+                    <div class="name">
+                      <p>露脸女朋友 大一女生爱露脸 直拨镜头 露脸女朋友 露脸女朋友</p>
+                      <button class="btn">关注</button>
+                    </div>
+                    <span>88888888关注<i class="dot"></i>2551视频</span>
+                  </div>
+                  <div class="interaction">
+                    <a href="#" class="btn">
+                      <i class="icon icon__message"></i>
+                      <span>1w</span>
+                    </a>
+                    <a href="#" class="btn">
+                      <i class="icon icon__share"></i>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
     </main>
     <van-tabbar v-model="active">
       <van-tabbar-item icon="wap-home">精选</van-tabbar-item>
@@ -107,6 +190,8 @@
 </template>
 
 <script>
+  import '@master/css';
+
   import { ref } from 'vue';
   import { createApp } from 'vue';
   import { Image as VanImage } from 'vant';
@@ -115,6 +200,9 @@
   app.use(VanImage);
 
   export default {
+    data() {
+      return { tab: 3 }
+    },
     setup() {
       const active = ref(0);
       return { active };
---
+++ public/Records.md
@@ -0,0 +1,5 @@
+--
+
+### 使用套件
+- Vant (https://vant-ui.github.io/vant/#/zh-CN)
+- Master CSS (https://css.master.co/tw)
