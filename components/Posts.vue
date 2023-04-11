9a66ccc0029b28c431d1b5b1e6db0ce08c8cc88b
chiuko <q8156697@gmail.com>
2023-04-11 14:19:37 +0800

    a

---
+++ components/Posts.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="list" v-infinite-scroll="loadMore">
+    <div class="item" v-for="n in 10">
+      <div class="info">
+        <div class="avatar">
+          <img class="img" src="https://api.bcyapp005.com/storage/files/shares/HH/3/f533de42-4806-4108-beec-612bc6e47b5c.jpg">
+        </div>
+        <div class="title">
+          <div class="name">
+            <p>露脸女朋友 大一女生爱露脸 直拨镜头 露脸女朋友 露脸女朋友</p>
+          </div>
+          <span>2023-03-07</span>
+        </div>
+      </div>
+      <div class="preview">
+        <div class="view"><span>5w</span>次播放</div>
+        <div class="time"><span>2:12</span></div>
+        <img class="img" src="https://api.bcyapp005.com/storage/files/shares/HH/3/f533de42-4806-4108-beec-612bc6e47b5c.jpg">
+      </div>
+      <div class="interaction">
+        <a href="#" class="btn">
+          <i class="icon icon__love"></i>
+          <span>14 132</span>
+        </a>
+        <a href="#" class="btn">
+          <i class="icon icon__message"></i>
+          <span>116</span>
+        </a>
+        <a href="#" class="btn">
+          <i class="icon icon__share"></i>
+          <span>分享</span>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const loadMore = () => {
+  console.log('more')
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
         <div class="featuredTab__list">
           <ul class="list">
             <li :class="featuredTab == n ? 'current' : ''" v-for="n in 10">
-              <a href="#" class="btn" @click="featuredTab = n">
+              <a href="#" class="btn" @click="featuredTabChange(n - 1)">
                 <span>精选 {{ n }}</span>
               </a>
             </li>
@@ -20,45 +20,14 @@
         <Transition appear>
           <swiper
             class="firstMainSwiper"
+            @swiper="onSwiper"
+            @slideChange="onSlideChange"
           >
             <swiper-slide>
               <div class="content">
                 <section class="section sectionVideoFocus">
                   <div class="featuredVideoFocus">
-                    <div class="list">
-                      <div class="item" v-for="n in 10">
-                        <div class="info">
-                          <div class="avatar">
-                            <img class="img" src="https://api.bcyapp005.com/storage/files/shares/HH/3/f533de42-4806-4108-beec-612bc6e47b5c.jpg">
-                          </div>
-                          <div class="title">
-                            <div class="name">
-                              <p>露脸女朋友 大一女生爱露脸 直拨镜头 露脸女朋友 露脸女朋友</p>
-                            </div>
-                            <span>2023-03-07</span>
-                          </div>
-                        </div>
-                        <div class="preview">
-                          <div class="view"><span>5w</span>次播放</div>
-                          <div class="time"><span>2:12</span></div>
-                          <img class="img" src="https://api.bcyapp005.com/storage/files/shares/HH/3/f533de42-4806-4108-beec-612bc6e47b5c.jpg">
-                        </div>
-                        <div class="interaction">
-                          <a href="#" class="btn">
-                            <i class="icon icon__love"></i>
-                            <span>14 132</span>
-                          </a>
-                          <a href="#" class="btn">
-                            <i class="icon icon__message"></i>
-                            <span>116</span>
-                          </a>
-                          <a href="#" class="btn">
-                            <i class="icon icon__share"></i>
-                            <span>分享</span>
-                          </a>
-                        </div>
-                      </div>
-                    </div>
+                    <Posts></Posts>
                   </div>
                 </section>
               </div>
@@ -87,39 +56,7 @@
                     <a href="#" class="btn btn__more">更多</a>
                   </div>
                   <div class="featuredVideo">
-                    <div class="list" v-infinite-scroll="loadMore" infinite-scroll-disabled="busy" infinite-scroll-distance="10">
-                      <NuxtLink class="item" v-for="n in 10" to="/_template/videoDetail">
-                        <div class="preview">
-                          <div class="title">
-                            <p>3/1露脸女朋友 大一女生爱露脸 直播镜头 露脸女朋友</p>
-                          </div>
-                          <div class="view"><span>5w</span>次播放</div>
-                          <div class="time"><span>2:12</span></div>
-                          <img class="img" src="https://api.bcyapp005.com/storage/files/shares/HH/3/f533de42-4806-4108-beec-612bc6e47b5c.jpg">
-                        </div>
-                        <div class="info">
-                          <div class="avatar">
-                            <img class="img" src="https://api.bcyapp005.com/storage/files/shares/HH/3/f533de42-4806-4108-beec-612bc6e47b5c.jpg">
-                          </div>
-                          <div class="title">
-                            <div class="name">
-                              <p>露脸女朋友 大一女生爱露脸 直拨镜头 露脸女朋友 露脸女朋友</p>
-                              <button class="btn btn__focus">关注</button>
-                            </div>
-                            <span>88888888关注<i class="dot"></i>2551视频</span>
-                          </div>
-                          <div class="interaction">
-                            <a href="#" class="btn">
-                              <i class="icon icon__message"></i>
-                              <span>1w</span>
-                            </a>
-                            <a href="#" class="btn">
-                              <i class="icon icon__share"></i>
-                            </a>
-                          </div>
-                        </div>
-                      </NuxtLink>
-                    </div>
+                    <Posts></Posts>
                   </div>
                 </section>
               </div>
@@ -131,39 +68,7 @@
                 </section>
                 <section class="section sectionVideo mt:10">
                   <div class="featuredVideo">
-                    <div class="list">
-                      <div class="item" v-for="n in 20">
-                        <div class="preview">
-                          <div class="title">
-                            <p>3/1露脸女朋友 大一女生爱露脸 直播镜头 露脸女朋友</p>
-                          </div>
-                          <div class="view"><span>5w</span>次播放</div>
-                          <div class="time"><span>2:12</span></div>
-                          <img class="img" src="https://api.bcyapp005.com/storage/files/shares/HH/3/f533de42-4806-4108-beec-612bc6e47b5c.jpg">
-                        </div>
-                        <div class="info">
-                          <div class="avatar">
-                            <img class="img" src="https://api.bcyapp005.com/storage/files/shares/HH/3/f533de42-4806-4108-beec-612bc6e47b5c.jpg">
-                          </div>
-                          <div class="title">
-                            <div class="name">
-                              <p>露脸女朋友 大一女生爱露脸 直拨镜头 露脸女朋友 露脸女朋友</p>
-                              <button class="btn btn__focus">关注</button>
-                            </div>
-                            <span>88888888关注<i class="dot"></i>2551视频</span>
-                          </div>
-                          <div class="interaction">
-                            <a href="#" class="btn">
-                              <i class="icon icon__message"></i>
-                              <span>1w</span>
-                            </a>
-                            <a href="#" class="btn">
-                              <i class="icon icon__share"></i>
-                            </a>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
+                    <Posts></Posts>
                   </div>
                 </section>
               </div>
@@ -178,12 +83,28 @@
 
 <script setup>
 import { useMainStore } from '@/stores/main'
-import { Controller } from 'swiper';
 const mainStore = useMainStore()
 const { onBodyScroll } = usePublic()
 
-const tab = ref(1)
+const mainSwiper = ref(null)
+const onSwiper = (swiper) => {
+  mainSwiper.value = swiper
+}
+
+const onSlideChange = (t) => {
+  featuredTab.value = t.activeIndex + 1
+}
+
+
 const featuredTab = ref(1)
+const featuredTabChange = (n) => {
+  featuredTab.value = n
+
+  if (mainSwiper != null) {
+    mainSwiper.value.slideTo(n)
+  }
+}
+
 const discoverMenu = ref(false)
 
 const loadMore = () => {
